17d346bd04b0533af641162867d084ff93e16e29
Mathis <mathis.reclus@gmail.com>
2022-01-25 08:47:42 +0100

    Generation de l'index pas finis

---
+++ src/main/java/GenerateHTML.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class GenerateHTML {
+    public void index() {
+
+        //Variable
+        FileManager fileManager = new FileManager();
+        fileManager.createArrayAgents();
+        Scanner scanner = new Scanner(System.in);
+        Scanner fileIn; //input file connection
+        PrintWriter fileOut; //HTML file connection
+        String line = null; // a line from the input file
+
+        File fileindex = new File("index.html");
+
+        try {
+            if (!fileindex.exists()) {
+                fileindex.createNewFile();
+            }
+        }catch (IOException e) {
+            // Print the exception
+            System.out.print(e.getMessage());
+        }
+        try {
+            fileOut = new PrintWriter(fileindex);
+
+            // Ouverture des TAGS HTML
+            fileOut.println("<html>");
+            fileOut.println("<head>");
+            fileOut.println("</head>");
+            fileOut.println("<body>");
+
+            //PRINT de la list d'agents
+            ArrayList<String> agents = fileManager.getListagents();
+            for (String string : agents) {
+                fileOut.println("   <br>");
+                fileOut.println("<a href="+string+".html>"+string+"</a>");
+                fileOut.println("   <br>");
+            }
+            // Fermeture des TAGS HTML
+            fileOut.println("</body>");
+            fileOut.println("</html>");
+
+            fileOut.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+    }
+}
+
+

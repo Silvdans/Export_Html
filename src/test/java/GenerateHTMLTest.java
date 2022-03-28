41cdd4a2be9ed01fb5f7523027104fa7afe70e44
= <loic.delpierre16@gmail.com>
2022-03-28 20:06:50 +0200

    tests

--- src/test/java/GenerateHTMLTest.java
+++ src/test/java/GenerateHTMLTest.java
@@ -1,13 +1,23 @@
 import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class GenerateHTMLTest {
+    @BeforeEach
+    public void deletefolder() throws IOException {
+        File file = new File("html");
+        if(file.exists()){
+            FileUtils.deleteDirectory(file);
+        }
+    }
     @Test
     public void testGenerateHtmlIndex() throws IOException {
         GenerateHTML gen = new GenerateHTML();
@@ -19,6 +29,7 @@ public class GenerateHTMLTest {
     public void testGenerateAgent() throws IOException{
         GenerateHTML gen = new GenerateHTML();
         String agent = gen.getFileManager().getListagents().get(1);
+        gen.createHtmlForEachAgents();
         File file = new File("html/"+agent+"/"+agent+".html");
         assertTrue(file.exists());
     }
